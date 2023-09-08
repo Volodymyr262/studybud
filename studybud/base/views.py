26e26476daf0e3c16360bbcfce1c4969e9de88b4
Volodymyr262 <volodiahedz26@gmail.com>
2023-09-08 12:01:57 +0200

    update_user view created, designed

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def loginPage(request):
@@ -135,7 +135,6 @@ def updateRoom(request, pk):
         topic, created = Topic.objects.get_or_create(name=topic_name)
         room.name = request.POST.get('name')
         room.topic = topic
-        #comment
         room.description = request.POST.get('description')
         room.save()
         return redirect('home')
@@ -163,3 +162,16 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':  message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update_user.html', {'form': form})
